e7c1813a487f9dc46bb23512cf6d7344b9b2eb43
hiralsinghal <singhal.hiral@gmail.com>
2026-01-18 02:18:38 +0530

    add 3js

---
+++ script.js
@@ -0,0 +1,40 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+renderer.render(scene, camera);
+document.body.appendChild(renderer.domElement);
+
+function add_star() {
+  const star_geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const star_material = new THREE.MeshBasicMaterial({ color: 0xffffff  });
+  const star = new THREE.Mesh(star_geometry, star_material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(200));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(add_star);
+
+camera.position.z = 3
+scene.add(camera);
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0000;
+  camera.rotation.y = t * -0.0000;
+
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
