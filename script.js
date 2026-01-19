c75330fcb33327df9233a62efd2f8c824de5b935
hiralsinghal <singhal.hiral@gmail.com>
2026-01-19 23:45:25 +0530

    added a song for fun

--- script.js
+++ script.js
@@ -43,3 +43,17 @@ function moveCamera() {
 
 document.body.onscroll = moveCamera;
 moveCamera();
+
+document.getElementById("playAudio").addEventListener("click", function(){
+    var audio = document.getElementById('testAudio');
+  if(this.className == 'is-playing'){
+    this.className = "";
+    this.innerHTML = "Play"
+    audio.pause();
+  }else{
+    this.className = "is-playing";
+    this.innerHTML = "Pause";
+    audio.play();
+  }
+
+})
